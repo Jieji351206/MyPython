60eab6b49b37ac320a04aa93165c67f3449400ac
Jieji351206 <6521061004@cdti.ac.th>
2023-10-30 13:26:33 +0700

    first commit

---
+++ Hello World.py
@@ -0,0 +1,10 @@
+print("Hello World")
+msg1 = 'สวัสดีค่ะ'
+msg2 = "MicroPython"
+
+print(msg1+ " " +msg2)
+
+name = "Pitchayatida Phungpis"
+print(name)
+print(name[8:12])
+print("-------------")
---
+++ Lab 1-3.py
@@ -0,0 +1,15 @@
+a = {2023, 15000, "Pitchayatida"}   #SET
+b = [2023, 15000, "Pitchayatida"]   #List
+c = (2023, 15000, "Pitchayatida")   #Tuple
+d = {1:2023, 'salary':15000, "name":"Pitchayatida"} #Dictionary
+
+b[1] = 25000
+d['salary']= 35000
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(d[1])
+print(d["name"])
+print(type(a))     #แสดงชนิดข้อมูล
